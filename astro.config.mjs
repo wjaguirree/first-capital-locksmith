899deb7777de497377dc56bc3891a154a2e5f530
Wendy Aguirre <wendy.aguirremarketing@gmail.com>
2026-07-22 10:31:31 -0400

    git commit -m "update made to astro.config.mjs"

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.1stcapitallocksmith.com',
+  site: 'https://www.1stcapitallocksmithde.com',
   trailingSlash: 'always',
   integrations: [sitemap()],
   vite: {
